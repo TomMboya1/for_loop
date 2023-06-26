99a1d12d8c1472643cd0bbdae8e314e538ac3cd7
Tom MBOYA <tommboya044@gmail.com>
2023-06-26 20:01:44 +0300

    1st commit

---
+++ src/main/java/org/example/for_loops.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class for_loops {
+    public static void main(String[] args) {
+        for (int i =5; i<=25; i++){
+            System.out.println(i);
+        }
+    }
+}
